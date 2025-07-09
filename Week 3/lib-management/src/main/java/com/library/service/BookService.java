a515b1572c245ce80b99da4c789d6c5912c86cae
Akshaya <158743650+Akshaya0618@users.noreply.github.com>
2025-07-09 22:48:05 +0530

    Add files via upload

---
+++ Week 3/lib-management/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,20 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+import java.util.List;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void displayBooks() {
+        List<String> books = bookRepository.getAllBooks();
+        System.out.println("Books available in repository:");
+        for (String book : books) {
+            System.out.println(book);
+        }
+    }
+}
